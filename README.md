5a8b9fb857276c6dddcd6ad2ed6ff558e0f576e9
bitmage <q@bitmage.net>
2016-09-25 23:39:06 -0700

    conversion to Vue

---
+++ README.md
@@ -0,0 +1,12 @@
+# Vue Bare
+
+A chat demo using Vue.  No build tools, just raw JS.  Server communication is mocked out in `public/chatApi.js`... Replace with Ajax calls to connect the chat to a real server.
+
+# Running
+
+Just serve the public directory.  You can use `node-static`:
+
+```bash
+npm install -g node-static
+static public
+```
---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+(function() {
+
+  Vue.component('main', {
+    template: '#main-template',
+    data: function() {
+      return {
+        name: '',
+      }
+    },
+    methods: {
+      setName: function(name) {
+        this.name = name;
+      }
+    },
+    created: function() {
+      // called via $dispatch by login
+      this.$on('name-set', this.setName);
+    },
+  });
+
+  new Vue({el: '#main'});
+
+})();
